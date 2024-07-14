451ee5719768684de4e83f7f7b3dbd818e8e246d
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-07-14 20:43:29 -0300

    wip

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { RegisterForm } from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/patient.actions";
 
-const RegisterPage = () => {
+const RegisterPage = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
   return (
-    <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
+    <div className="flex min-h-screen">
+      <section className="remove-scrollbar container">
         <div className="sub-container max-w-[496px]">
           <Image
             src={"/assets/icons/logo-full.svg"}
@@ -15,7 +18,7 @@ const RegisterPage = () => {
             className="mb-12 h-10 w-fit"
           />
 
-          {/* <PatientForm /> */}
+          <RegisterForm user={user} />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="text-dark-600 justify-items-end xl:text-left">
               &copy; 2024 CarePulse
@@ -29,10 +32,10 @@ const RegisterPage = () => {
 
       <Image
         src={"/assets/images/register-img.png"}
-        height={1000}
+        height={560}
         width={1000}
         alt="patient"
-        className="side-img max-w-[390px]"
+        className=" md:max-w-[350px] h-screen lg:max-w-[550px]"
       />
     </div>
   );
--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -10,19 +10,10 @@ import { SubmitButton } from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { createUser, getUser } from "@/lib/actions/patient.actions";
+import { FormFieldType } from "./PatientForm";
 
-export enum FormFieldType {
-  INPUT = "input",
-  TEXTAREA = "textarea",
-  PHONE_INPUT = "phoneInput",
-  CHECKBOX = "checkbox",
-  DATE_PICKER = "datePicker",
-  SELECT = "select",
-  SKELETON = "skeleton",
-}
-
-export const PatientForm = () => {
+export const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -52,39 +43,49 @@ export const PatientForm = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">Hi there 👋</h1>
-          <p className="text-dark-700">Schedule your first appointment.</p>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12 flex-1"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">Let us know more about yourself.</p>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
         </section>
 
         <CustomFormField
           control={form.control}
           fieldType={FormFieldType.INPUT}
           name="name"
-          label="Full Name"
           placeholder="John Doe"
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
 
-        <CustomFormField
-          control={form.control}
-          fieldType={FormFieldType.INPUT}
-          name="email"
-          label="Email"
-          placeholder="johndoe@jsmastery.pro"
-          iconSrc="/assets/icons/email.svg"
-          iconAlt="email"
-        />
+        <div className="flex max-xl:flex-col gap-6">
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.INPUT}
+            name="email"
+            label="Email"
+            placeholder="johndoe@jsmastery.pro"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
 
-        <CustomFormField
-          control={form.control}
-          fieldType={FormFieldType.PHONE_INPUT}
-          name="phone"
-          label="Phone number"
-          placeholder="(555) 123-4567"
-        />
+          <CustomFormField
+            control={form.control}
+            fieldType={FormFieldType.PHONE_INPUT}
+            name="phone"
+            label="Phone number"
+            placeholder="(555) 123-4567"
+          />
+        </div>
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
